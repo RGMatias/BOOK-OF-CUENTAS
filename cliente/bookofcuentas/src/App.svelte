3c9acd8b99f6e4a7cd8930476e687022c50f9483
darkkwing <dargamarr@gmail.com>
2023-10-30 20:46:22 -0300

    Cuerpo
    Se agrego cuerpo de la pagina

--- cliente/bookofcuentas/src/App.svelte
+++ cliente/bookofcuentas/src/App.svelte
@@ -1,9 +1,17 @@
 <script>
-
+    import Header from "./lib/Header.svelte";
+    import Body from "./lib/Body.svelte";
+    import Footer from "./lib/Footer.svelte";
+  
 
 </script>
 
 <main>
+  <Header/>
+  <Body/>
+  <Footer/>
+
+
  
 </main>
 
