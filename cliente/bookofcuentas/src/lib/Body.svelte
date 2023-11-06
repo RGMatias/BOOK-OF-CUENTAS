2852c3a1a648002052c8c9dd7358aef56d88bd42
darkkwing <dargamarr@gmail.com>
2023-11-05 23:06:21 -0300

    body
    inicio

--- cliente/bookofcuentas/src/lib/Body.svelte
+++ cliente/bookofcuentas/src/lib/Body.svelte
@@ -1,9 +1,36 @@
 <script>
-    // your script goes here
+   
 </script>
-<div>
 
+<div>
+    <div class="container">
+        <div>Fecha</div>
+        <div>Informacion</div>
+        <div>Tipo</div>
+        <div>Monto</div>
+        <div>Button</div>
+    </div>
 </div>
+
 <style>
+    .container{
+        margin: 1em 1em 1em 1em;
+        width: 117em;
+        height: 45em;
+        flex-shrink: 0;
+        border-radius: 25px;
+        border: 5px solid #000;
+        background: #FFEEDE;
+        mix-blend-mode: soft-light;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        display: grid;
+        grid-template-columns: auto auto auto auto auto;
+
+    }
+
+   
+@media only screen and (max-width: 800px) {
     
+
+}
 </style>
