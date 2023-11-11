becbd13e4c0d1dff98904ccc3f6d14cc372c86bc
RGMatias <141976307+RGMatias@users.noreply.github.com>
2023-11-11 16:48:43 -0300

    config de Base de datos
    falta .env

---
+++ servidor/DB/config.js
@@ -0,0 +1,10 @@
+const mongooose = require("mongoose")
+const DBConnection=()=>{
+    try {
+        mongooose.set('strictQuery', false);
+        mongoose.connect(process.env.BD)
+        console.log("Se ha conctado a la Base de datos correctamente")
+    } catch (error) {
+        console.log("Error al conectarce a la Base de datos")
+    }
+}
