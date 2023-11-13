51611f2b03426a15b46d846d7a39abceef1ca1e7
RGMatias <141976307+RGMatias@users.noreply.github.com>
2023-11-13 18:24:14 -0300

    arreglo de base de datos

--- servidor/DB/config.js
+++ servidor/DB/config.js
@@ -1,13 +1,11 @@
-const mongooose = require("mongoose")
+const mongoose = require("mongoose")
 const DBConnection=async ()=>{
     try {
-        mongooose.set('strictQuery', false);
+        mongoose.set('strictQuery', false);
     await    mongoose.connect(process.env.BD)
         console.log("Se ha conectado a la Base de datos correctamente")
     } catch (error) {
         console.log("Error al conectarse a la Base de datos")
     }
 }
-module.exports={
-    DBConnection
-}
+module.exports={DBConnection}
