1f56bb5b118e9fe6c35440a6c1f6c4aaea25603d
RGMatias <141976307+RGMatias@users.noreply.github.com>
2023-11-21 17:12:36 -0300

    Relog en milisegundos

--- cliente/bookofcuentas/src/lib/Relog.svelte
+++ cliente/bookofcuentas/src/lib/Relog.svelte
@@ -2,6 +2,12 @@
     const Relog = new Date().toLocaleDateString()
     const fecha = Relog
     console.log(fecha)
+
+    // let date = new Date()
+    // let currentTime = date.getTime()
+    // console.log(currentTime)
+    // let newDate = new Date(currentTime)
+    // console.log(newDate)
     
 </script>
 <div >
