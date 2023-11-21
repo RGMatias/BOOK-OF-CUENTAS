ce9622202a04258aa41224b7fe4a9bee4bfd4a4c
RGMatias <141976307+RGMatias@users.noreply.github.com>
2023-11-20 23:49:25 -0300

    Relog
    prueba uno de hacer fecha y hora

---
+++ cliente/bookofcuentas/src/lib/Relog.svelte
@@ -0,0 +1,11 @@
+<script>
+    export const Relog = new Date().toLocaleDateString()
+    const fecha = Relog
+    console.log(fecha)
+</script>
+<div class="{fecha}">
+
+</div>
+<style>
+    
+</style>
